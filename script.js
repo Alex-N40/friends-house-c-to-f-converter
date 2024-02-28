c03b9e983a41790bfbff91c42aeb6484cf1a960a
Alex-N40 <alex_n40@proton.me>
2024-02-28 13:02:07 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+console.log(
+  `Outside my friend’s house it’s ${tempCelsius}°C, which is ${toFahrenheit}°F.`
+);
+
+/* Result in console:
+Outside my friend’s house it’s 10°C, which is 50°F. */
